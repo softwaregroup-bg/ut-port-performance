13f3b403c2fbe57932cb71463e2e39be80e15a86
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-07-06 17:35:32 +0300

    feat: send only measurements that have been updated fix: remove unnecesarry inheritance model as the code has been modified not to rely to events

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ module.exports = function(Parent) {
         var suffix = ' ' + Date.now() + '000000';
         return Object.keys(this.measurements).map((measurement) => {
             return this.measurements[measurement].influx(deltaTime, tags, suffix);
-        });
+        }).filter(x => x);
     };
 
     PerformancePort.prototype.stats = function stats(tags) {
@@ -52,7 +52,7 @@ module.exports = function(Parent) {
         var suffix = ' ' + Date.now() + '000000';
         return Object.keys(this.measurements).map((measurement) => {
             return this.measurements[measurement].statsD(deltaTime, tags, suffix);
-        });
+        }).filter(x => x);
     };
 
     PerformancePort.prototype.start = function start() {
